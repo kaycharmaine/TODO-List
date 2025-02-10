26b9ef9bea09dcb290c2f75816e3bda5c3745fd2
Kgothatso <kgothatsokgaba7@gmail.com>
2025-02-10 15:32:16 +0200

    Project initiated

---
+++ index.js
@@ -0,0 +1,28 @@
+let tasks = [];
+
+const tasksDiv = document.getElementById('tasks');
+
+function loadTaks() 
+{
+
+}
+
+function renderTaks() 
+{
+    
+}
+
+function saveTaks() 
+{
+    
+}
+
+function addTak() 
+{
+    
+}
+
+function removeTaks() 
+{
+    
+}
